a9b65b212475c578f7220be85f19a3e147049a08
LuYaHu <1210797561@qq.com>
2023-01-31 04:21:44 +0000

    连接Mysql

--- graduate_project/graduate_project/settings.py
+++ graduate_project/graduate_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q&)!ag2!q+aijv-dx45y@7g7#!$ro^$byy_dljd^i(f057wx-*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.3.6']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'graduate_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'graduate_project',
+        'HOST':'127.0.0.1',
+        'POST':3306,
+        'USER':'root',
+        'PASSWORD':'20010831'
     }
 }
 
